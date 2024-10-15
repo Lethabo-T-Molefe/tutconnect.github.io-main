8986d08dfc0580f07b7696d48d61db5e331095ae
Lethabo-T-Molefe <lethabomolefe69@gmail.com>
2024-10-15 12:09:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
+const loginUsers = {
+  "studentNumber" : 123456789,
+  "password" : "hello"
+};
+
 menu.addEventListener('click', function() {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
